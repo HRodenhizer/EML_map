e4f4f176f6a022b7379bcffbda9fa3c3a9a2bddc
Heidi R <hgr7@nau.edu>
2019-10-24 16:07:57 -0700

    add plot locations and allow site data to be toggled on and off

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(leaflet)
 
 ### Load data ###
 sites <- st_read('data/EML_Sites.shp')
+plots <- st_read('data/plot_coords.shp')
 
 ### Source helper functions ###
 source('helpers.R')
@@ -15,7 +16,8 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      
+      checkboxInput("site", label = "Sites", value = TRUE)
+       
     ),
     
     mainPanel(
@@ -26,12 +28,29 @@ ui <- fluidPage(
 
 # Server logic ----
 server <- function(input, output) {
+  
+  icon.glyphicon <- makeAwesomeIcon(icon= 'map-marker-alt', library = 'fa', markerColor = 'lightgray', iconColor = 'black')
+  pal <- colorFactor(palette = c("black", "red", 'yellow'), domain = plots$exp)
+  
   output$map <- renderLeaflet({
+    sitedata <- switch(
+      input$site,
+      'Sites' = sites
+    )
+    if (input$site) {
     leaflet() %>%
       addTiles() %>%
-      addMarkers(data = sites,
-                 label = sites$Name) %>% 
+      addAwesomeMarkers(data = sitedata,
+                 label = sitedata$Name,
+                 icon = icon.glyphicon) %>%
+      addCircles(data = plots, color = ~pal(plots$exp), radius = 0.5, label = plots$label) %>%
       addProviderTiles("Esri.WorldImagery")
+    } else {
+      leaflet() %>%
+        addTiles() %>%
+        addCircles(data = plots, color = ~pal(plots$exp), radius = 0.5, label = plots$label) %>%
+        addProviderTiles("Esri.WorldImagery")
+    }
   })
   
 }
--- build_map.R
+++ build_map.R
@@ -1,11 +1,38 @@
 library(sf)
 library(leaflet)
+library(tidyverse)
 
 data <- st_read("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Shiny/EML_map/data/EML_Sites.shp")
 # data <- data %>%
 #   st_transform(4326)
 
-# st_write(data, 'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Shiny/EML_map/data/EML_Sites.shp')
+data <- st_read("C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/GPS/All_Points/All_Points_2017_SPCSAK4.shp")
+
+data <- data %>%
+  st_transform(4326) %>%
+  filter(as.character(Type) == 'grad' | as.character(Type) == 'flux' | as.character(Type) == 'gw' | as.character(Type) == 'ww') %>%
+  select(Name, Type) %>%
+  st_zm() %>%
+  mutate(Name = as.character(Name),
+         Name = ifelse(str_detect(Name, pattern = '^grad'),
+                       paste(str_sub(Name, 1, 4), str_sub(Name, 5), sep = '.'),
+                       Name)) %>%
+  separate(Name, c('fence', 'plot'), extra = 'merge') %>%
+  mutate(fence = as.numeric(ifelse(as.character(Type) == 'ww',
+                        str_sub(fence, 3),
+                        fence)),
+         plot = ifelse(as.character(Type) == 'flux' | as.character(Type) == 'gw',
+                       str_sub(1, 1),
+                       plot),
+         exp = ifelse(as.character(Type) == 'grad',
+                      'Gradient',
+                      ifelse(!is.na(as.numeric(plot)),
+                             'CiPEHR',
+                             'DryPEHR')),
+         label = ifelse(exp == 'Gradient',
+                        paste(exp, '/n', 'Plot: ', plot),
+                        paste(exp, Type, '/n', 'Fence: ', fence, ', Plot: ', plot)))
+# st_write(data, 'C:/Users/Heidi Rodenhizer/Documents/School/NAU/Schuur Lab/Shiny/EML_map/data/plot_coords.shp')
 
 leaflet(data) %>%
   addTiles() %>%
